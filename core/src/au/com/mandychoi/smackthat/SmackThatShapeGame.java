3005321894906ca7acb23271c9b078250c5dd3c3
mandy <mandy@wefarm.com>
2023-05-20 19:29:20 +0100

    Initial commit - blank libgdx project

---
+++ core/src/au/com/mandychoi/smackthat/SmackThatShapeGame.java
@@ -0,0 +1,26 @@
+package au.com.mandychoi.smackthat;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class SmackThatShapeGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 1);
+		batch.begin();
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
