0979da9bef1bfb2cd594a507b9af202d883a4e92
mandy <mandy@wefarm.com>
2023-05-21 02:03:50 +0100

    Move sprite into Shape

--- core/src/au/com/mandychoi/smackthat/Shape.java
+++ core/src/au/com/mandychoi/smackthat/Shape.java
@@ -1,15 +1,28 @@
 package au.com.mandychoi.smackthat;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.MathUtils;
 
 public class Shape {
     public static final int SHAPE_SIZE = 64;
+
+    private static final TextureAtlas atlas =
+            new TextureAtlas(Gdx.files.internal("simple_shapes.atlas"));
+    private static final Sprite sprite = atlas.createSprite("circle_filled");
+
     private int x;
     private int y;
+    private int width;
+    private int height;
 
     public Shape() {
         x = MathUtils.random(0, GameLevelScreen.LEVEL_WIDTH - SHAPE_SIZE);
         y = GameLevelScreen.LEVEL_HEIGHT;
+        width = SHAPE_SIZE;
+        height = SHAPE_SIZE;
     }
 
     public void drop(float distance) {
@@ -22,6 +35,9 @@ public class Shape {
     }
 
     public void draw(SmackThatShapeGame game) {
-        game.draw(x, y);
+        sprite.setPosition(x, y);
+        sprite.setColor(Color.BLUE);
+        sprite.setSize(width, height);
+        game.draw(sprite);
     }
 }
--- core/src/au/com/mandychoi/smackthat/SmackThatShapeGame.java
+++ core/src/au/com/mandychoi/smackthat/SmackThatShapeGame.java
@@ -1,24 +1,15 @@
 package au.com.mandychoi.smackthat;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class SmackThatShapeGame extends Game {
     private SpriteBatch batch;
-    private Sprite sprite;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("simple_shapes.atlas"));
-
-        sprite = atlas.createSprite("circle_filled");
-        sprite.setSize(64, 64);
-
         this.setScreen(new GameLevelScreen(this));
     }
 
@@ -40,9 +31,7 @@ public class SmackThatShapeGame extends Game {
         batch.end();
     }
 
-    public void draw(float x, float y) {
-        sprite.setPosition(x, y);
-        sprite.setColor(Color.BLUE);
+    public void draw(Sprite sprite) {
         sprite.draw(batch);
     }
 }
