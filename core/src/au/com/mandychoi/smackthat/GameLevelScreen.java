c09b2c49194d3a7a0b2defbf75151a2406e372a8
mandy <mandy@wefarm.com>
2023-05-21 01:26:20 +0100

    Spawn falling ciricles

--- core/src/au/com/mandychoi/smackthat/GameLevelScreen.java
+++ core/src/au/com/mandychoi/smackthat/GameLevelScreen.java
@@ -1,29 +1,75 @@
 package au.com.mandychoi.smackthat;
 
+import java.util.Iterator;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class GameLevelScreen implements Screen {
     private final SmackThatShapeGame game;
 
+    private Array<Rectangle> shapes;
+    private long lastSpawnTime;
+    private final int shapeSize = 64;
+    private final int levelWidth = 800;
+    private final int levelHeight = 480;
+
     public GameLevelScreen(SmackThatShapeGame game) {
         this.game = game;
+        shapes = new Array<Rectangle>();
     }
 
     @Override
-    public void show() {
-    }
+    public void show() {}
 
     @Override
     public void render(float delta) {
+        // process input
+
+        update();
+        draw();
+    }
+
+    private void update() {
+        if (TimeUtils.nanoTime() - lastSpawnTime > 1000000000)
+            spawnShape();
+
+        Iterator<Rectangle> iter = shapes.iterator();
+        while (iter.hasNext()) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if (raindrop.y + shapeSize < 0)
+                iter.remove();
+        }
+    }
+
+    private void spawnShape() {
+        Rectangle shape = new Rectangle();
+        shape.x = MathUtils.random(0, levelWidth - shapeSize);
+        shape.y = levelHeight;
+        shape.width = shapeSize;
+        shape.height = shapeSize;
+        shapes.add(shape);
+        lastSpawnTime = TimeUtils.nanoTime();
+    }
+
+    private void draw() {
+        // draw
         ScreenUtils.clear(0, 0, 0, 1);
         game.beginRendering();
+        for (Rectangle shape : shapes) {
+            game.draw(shape.x, shape.y);
+        }
         game.endRendering();
     }
 
     @Override
-    public void resize(int width, int height) {
-    }
+    public void resize(int width, int height) {}
 
     @Override
     public void pause() {
@@ -41,6 +87,5 @@ public class GameLevelScreen implements Screen {
     }
 
     @Override
-    public void dispose() {
-    }
+    public void dispose() {}
 }
--- core/src/au/com/mandychoi/smackthat/SmackThatShapeGame.java
+++ core/src/au/com/mandychoi/smackthat/SmackThatShapeGame.java
@@ -1,14 +1,24 @@
 package au.com.mandychoi.smackthat;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class SmackThatShapeGame extends Game {
     private SpriteBatch batch;
+    private Sprite sprite;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("simple_shapes.atlas"));
+
+        sprite = atlas.createSprite("circle_filled");
+        sprite.setSize(64, 64);
+
         this.setScreen(new GameLevelScreen(this));
     }
 
@@ -28,5 +38,11 @@ public class SmackThatShapeGame extends Game {
 
     public void endRendering() {
         batch.end();
+    }
+
+    public void draw(float x, float y) {
+        sprite.setPosition(x, y);
+        sprite.setColor(Color.BLUE);
+        sprite.draw(batch);
     }
 }
